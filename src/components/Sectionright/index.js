b9a5944bb1b7978920d92113a0659d448e6b3090
Rianakeni <s22210372@student.unklab.ac.id>
2024-11-13 00:12:50 +0800

    adding firebase to section right

--- src/components/Sectionright/index.js
+++ src/components/Sectionright/index.js
@@ -1,29 +1,36 @@
-// import profile from "../../assets/img/profile.jpg";
+import { useEffect, useState } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 const SectionRight = () => {
+  const [sectionRight, setSectionRight] = useState({});
+
+  useEffect(() => {
+    const db = getDatabase();
+    const sectionRightRef = ref(db, "sectionRight/");
+    onValue(sectionRightRef, (snapshot) => {
+      const data = snapshot.val();
+      setSectionRight(data);
+    });
+  }, []);
+
   return (
     <section className="profile">
       <div className="description">
-        <h1>Expertise</h1>
-        <h2>Data Analyst</h2>
-        <p>
-          Exceptionally skilled in uncovering trends, patterns, and compelling
-          narratives hidden within complex datasets, capable of transforming raw
-          information into meaningful insights that drive strategic
-          decision-making and innovation.
-        </p>
+        <h1>{sectionRight.title2}</h1>
+        <h2>{sectionRight.expertise}</h2>
+        <p>{sectionRight.skilldescription}</p>
         <a href="https://wa.me/628995531878">
           <button>Hire me</button>
         </a>
         <div className="stats">
           <p>
-            <strong>1 Years</strong> Experience
+            <strong>{sectionRight.years}</strong> Experience
           </p>
           <p>
-            <strong>100+</strong> Clients
+            <strong>{sectionRight.clients}</strong> Clients
           </p>
           <p>
-            <strong>99.8%</strong> Satisfaction
+            <strong>{sectionRight.satisfaction}</strong> Satisfaction
           </p>
         </div>
       </div>
