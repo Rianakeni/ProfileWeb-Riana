a39f369456cfb3f3a84308c85952d95dd4e676c8
Rianakeni <s22210372@student.unklab.ac.id>
2024-11-13 01:14:02 +0800

    adding image to firebase

--- src/components/SectionMiddle/index.js
+++ src/components/SectionMiddle/index.js
@@ -1,10 +1,26 @@
 import profile from "../../assets/img/profile.png";
+import { useEffect, useState } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 const Sectionmiddle = () => {
+  const [sectionMiddle, setSectionMiddle] = useState({});
+
+  useEffect(() => {
+    const db = getDatabase();
+    const sectionMiddleRef = ref(db, "sectionMiddle/");
+    onValue(sectionMiddleRef, (snapshot) => {
+      const data = snapshot.val();
+      setSectionMiddle(data);
+    });
+  }, []);
+
   return (
     <div>
       <div className="profile-picture">
-        <img src={profile} alt="ProfilePicture" />
+        <img
+          src={`data:image/jpeg;base64, ${sectionMiddle.images}`}
+          alt="ProfilePicture"
+        />
       </div>
     </div>
   );
