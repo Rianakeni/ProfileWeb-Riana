8a666549ca065dec400aba95eb7a5be044503be3
Rianakeni <s22210372@student.unklab.ac.id>
2024-11-13 00:53:51 +0800

    fixing error in deploy

--- src/components/SectionMiddle/index.js
+++ src/components/SectionMiddle/index.js
@@ -1,10 +1,10 @@
-import profile from "../../assets/img/Profile.png";
+import profile from "../../assets/img/profile.png";
 
 const Sectionmiddle = () => {
   return (
     <div>
       <div className="profile-picture">
-        <img src={profile} alt="Profile Picture" />
+        <img src={profile} alt="ProfilePicture" />
       </div>
     </div>
   );
